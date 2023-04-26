26bef0ea68be8fb36ae65dfbe4d9dbf0b520d282
Santiago Cano <scano@hematologico.com>
2023-04-26 11:16:29 -0500

    Fix for reading csv file.

--- CovidDataService.py
+++ CovidDataService.py
@@ -1,14 +1,13 @@
-from abc import ABC, abstractmethod
 
 class CovidDataService:
     def __init__(self, data):
         self.data = data
 
-    def get_country_data(self, country):
+    def get_countries_data(self, country):
         return [d for d in self.data if d['country'] == country]
 
-    def get_country_historic_data(self, country, start_date, end_date):
-        country_data = self.get_country_data(country)
+    def get_countries_historic_data(self, country, start_date, end_date):
+        country_data = self.get_countries_data(country)
         filtered_data = []
         for d in country_data:
             if d['start_date'] <= end_date and d['end_date'] >= start_date:
--- app.py
+++ app.py
@@ -1,19 +1,47 @@
 from CovidDataService import CovidDataService
+from plotter import CSVPlotter, JSONPlotter, PlotterManager
+import pandas as pd
+
 
 countries = ['United States', 'Brazil', 'India', 'Russia', 'China', 'United Kingdom']
 covid_service = CovidDataService(countries)
 
 # Get data for more countries
-data = covid_service.get_country_data(countries)
+data = covid_service.get_countries_data(countries)
 print(data)
 
 # Get historical data for more countries and dates
 start_date1 = '2021-10-01'
 end_date1 = '2022-03-31'
-historical_data1 = covid_service.get_country_historic_data(countries, start_date1, end_date1)
+historical_data1 = covid_service.get_countries_historic_data(countries, start_date1, end_date1)
 print(historical_data1)
 
 start_date2 = '2022-04-01'
 end_date2 = '2022-09-30'
-historical_data2 = covid_service.get_country_historic_data(countries, start_date2, end_date2)
+historical_data2 = covid_service.get_countries_historic_data(countries, start_date2, end_date2)
 print(historical_data2)
+
+# Convert to Pandas DataFrame
+df = pd.DataFrame(data)
+
+# Save to CSV file
+df.to_csv('countries_data.csv', index=False)
+
+# Convert to Pandas DataFrame
+df = pd.DataFrame(historical_data1)
+
+# Save to CSV file
+df.to_csv('historical_data1.csv', index=False)
+
+# create a PlotterManager instance
+plotter_manager = PlotterManager('data_covid.json')
+
+# create a CSVPlotter and a JSONPlotter instance
+csv_plotter = CSVPlotter(covid_service)
+json_plotter = JSONPlotter(covid_service)
+
+# plot the data using the PlotterManager and the CSVPlotter
+plotter_manager.plot(csv_plotter)
+
+# plot the data using the PlotterManager and the JSONPlotter
+plotter_manager.plot(json_plotter)
